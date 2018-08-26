18feff95d8b83d7ef761e88d81d42cce38684c47
soob <dltntnqo@naver.com>
2018-08-26 16:03:36 +0900

    Created web crawling and saved in DB

--- yellowBot/admin.py
+++ yellowBot/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import realtimedata
 
 # Register your models here.
+## 아래의 코드를 입력하면 realtimedata를 admin 페이지에서 관리할 수 있습니다.
+admin.site.register(realtimedata)
--- yellowBot/models.py
+++ yellowBot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class realtimedata(models.Model):
+    title = models.CharField(max_length=200)
+    link = models.URLField()
+    
